a0c88cf0b76d0eb98cf28d628d696010b5870f01
Ekaterina <kattekly@mail.ru>
2022-09-15 11:17:29 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import {Accordion} from "./components/Accordion/Accordion";
+import {Raiting} from "./components/Raiting/Raiting";
+
+
+function App() {
+    console.log("App rendered")
+    return (
+        <div>
+            <PageTitle title={"App component"} />
+            <PageTitle title={"User"} />
+            <PageTitle title={"My friend"} />
+            Article 1
+            <Raiting value={3} />
+            <Accordion titleValue={"Menu"} collapsed={true} />
+            <Accordion titleValue={"Users"} collapsed={false} />
+            Article 2
+            <Raiting value={0} />
+            <Raiting value={1} />
+            <Raiting value={2} />
+            <Raiting value={3} />
+            <Raiting value={4} />
+            <Raiting value={5} />
+        </div>
+    );
+}
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    debugger
+    console.log("PageTitle rendered")
+    return <h1>{props.title}</h1>
+}
+
+
+
+export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+    collapsed: boolean
+}
+
+export function Accordion(props: AccordionPropsType) {
+    console.log("Accordion rendered")
+    if (props.collapsed === false) {
+        return <div>
+            <AccordionTitle title={props.titleValue} />
+            <AccordionBody />
+
+        </div>
+    }
+   else {
+       return <div>
+           <AccordionTitle title={props.titleValue} />
+
+       </div>
+    }
+
+}
+
+type AccordionTitlePropsType = {
+    title : string
+}
+
+function AccordionTitle (props: AccordionTitlePropsType) {
+    console.log("AccordionTitle rendered")
+    return <h3>{props.title}</h3>
+
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendered")
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
