7a5129eb4865f3d6a4baefb0dd77ba9983ab43ef
Ekaterina <kattekly@mail.ru>
2022-10-27 09:52:35 +0300

    Lessons-15

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,3 +1,6 @@
+import {inspect} from "util";
+import styles from "./Select.module.css"
+import {useState} from "react";
 
 type SelectPropsType = {
     value?: any
@@ -13,12 +16,22 @@ type ItemType = {
 
 export function Select (props: SelectPropsType) {
     const TitleItems = props.items.find(t => t.value === props.value)
-
+const [active, setActiv] = useState(false)
 
 return (
-    <div>
+    <>
+   {/*<select>
+       <option value="">Minsk</option>
+       <option value="">Moscow</option>
+       <option value="">Kiev</option>
+   </select>*/}
+
+    <div className={styles.select + " " + styles.active}>
         <h3>{TitleItems && TitleItems.title}</h3>
+        <div className={styles.items}>
         {props.items.map(el => <div key={el.value}>{el.title}</div>)}
+        </div>
     </div>
+    </>
 )
 }
