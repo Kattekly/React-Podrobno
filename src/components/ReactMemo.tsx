0da331084caddb9e1035f74198ee93344030499f
Ekaterina <kattekly@mail.ru>
2022-10-31 15:28:49 +0300

    Lessons-17

--- src/components/ReactMemo.tsx
+++ src/components/ReactMemo.tsx
@@ -4,18 +4,28 @@ const Counter = (props: { count: number }) => {
     return <div>{props.count}</div>
 }
 
-const User = (props: { users: Array<string> }) => {
+const UserSecret = (props: { users: Array<string> }) => {
+    console.log("HEHEHELLO")
     return <div>{props.users.map((u, i) => <div key={i}>{u}</div>)}</div>
 }
+
+const Users = React.memo(UserSecret)
 
 export const ReactMemo = () => {
     const [counter, setCounter] = useState(0)
     const [users, setUsers] = useState(['Kate', 'Lera', 'Vlad'])
+
+    const addUser = () => {
+       const newUser = [...users, 'Sveta ' + new Date().getTime()]
+        setUsers(newUser)
+    }
+
     return (
         <div>
             <button onClick={() => setCounter(counter + 1)}>+</button>
+            <button onClick={addUser}>add</button>
             <Counter count={counter}/>
-            <User users={users}/>
+            <Users users={users}/>
         </div>
     );
 };
