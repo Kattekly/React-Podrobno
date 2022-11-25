aced8851d618356c016ce56c1c7d0d40d8a00318
Ekaterina <kattekly@mail.ru>
2022-11-25 13:17:18 +0300

    Lessons-22

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,12 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Clock = () => {
-    let [timer, setTimer]= useState<number | undefined>(undefined)
+   function restoreState<T>(key: string, defaultState: T) {
+        let state = defaultState
+        const stateAsString = localStorage.getItem(key)
+        if (stateAsString !== null) state = JSON.parse(stateAsString) as T
+        return state
+    }
+    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
+    const stringTime = new Intl.DateTimeFormat('ru', {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    }).format(date)
+
+
+    useEffect( () => {
+        const id: number = window.setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+    })
 
 
     return (
         <div>
-
+            <strong>{stringTime}</strong>
         </div>
     );
 };
