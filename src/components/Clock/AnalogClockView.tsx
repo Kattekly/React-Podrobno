2482537ff6c68d3c5a5c11dcfdc27b5c65850076
Ekaterina <kattekly@mail.ru>
2022-11-30 13:51:03 +0300

    Lessons-24

--- src/components/Clock/AnalogClockView.tsx
+++ src/components/Clock/AnalogClockView.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {ClockViewPropsType} from "./Clock2";
-import "./AnalogClock.css"
+import s from "./AnalogClock.module.css"
 
 export const AnalogClockView:  React.FC<ClockViewPropsType> = ({date}) => {
 
@@ -15,11 +15,11 @@ export const AnalogClockView:  React.FC<ClockViewPropsType> = ({date}) => {
     };
 
 
-    return  <div className={"clock"}>
-        <div className={"analog-clock"}>
-            <div className={"dial seconds"} style={secondsStyle} />
-            <div className={"dial minutes"} style={minutesStyle} />
-            <div className={"dial hours"} style={hoursStyle} />
+    return  <div className={s.clock}>
+        <div className={s["analog-clock"]}>
+            <div className={`${s.dial} ${s.seconds}`} style={secondsStyle} />
+            <div className={`${s.dial} ${s.minutes}`} style={minutesStyle} />
+            <div className={`${s.dial} ${s.hours}`}style={hoursStyle} />
         </div>
     </div>
 }
